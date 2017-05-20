296365a04debe9a68c7602e23e99594c54885e38
jianming.wu <jianming.wu@yingzt.com>
2017-05-20 12:38:12 +0800

    finish day 2 part 2

--- day2.py
+++ day2.py
@@ -5,6 +5,23 @@ import sys
 import numpy as np
 
 
+BUTTON_NUMBER_MAP1 = {
+    '(-1, 1)' : 1, '(0, 1)' : 2, '(1, 1)' : 3,
+    '(-1, 0)' : 4, '(0, 0)' : 5, '(1, 0)' : 6,
+    '(-1, -1)': 7, '(0, -1)': 8, '(1, -1)': 9,
+}
+
+BUTTON_NUMBER_MAP2 = {
+                                   '(0, 2)' :  1 ,
+                  '(-1, 1)' :  2 , '(0, 1)' :  3 , '(1, 1)' :  4 ,
+    '(-2, 0)': 5, '(-1, 0)' :  6 , '(0, 0)' :  7 , '(1, 0)' :  8 , '(2, 0)': 9,
+                  '(-1, -1)': 'A', '(0, -1)': 'B', '(1, -1)': 'C',
+                                   '(0, -2)': 'D',
+}
+
+BUTTON_NUMBER_MAP = BUTTON_NUMBER_MAP2
+
+
 def main():
     """入口"""
     current_position = np.array((0, 0))
@@ -17,7 +34,7 @@ def main():
         for direction_char in instruction:
             direction = translate_char_to_direction(direction_char)
             current_position += direction
-            current_position = correct_position(current_position)
+            current_position = correct_position(current_position, direction)
 
         print translate_position_to_button_number(current_position)
 
@@ -34,17 +51,15 @@ def translate_char_to_direction(direction_char):
     return direction_map[direction_char]
 
 
-def correct_position(position):
+def correct_position(position, last_direction):
     """如果超出范围，修正位置"""
-    correction_map = {-2: -1, 2: 1}
-
     x, y = position
-    if x in correction_map:
-        x = correction_map[x]
-    if y in correction_map:
-        y = correction_map[y]
+    position_str = '(%d, %d)' % (x, y)
 
-    return np.array((x, y))
+    if BUTTON_NUMBER_MAP.get(position_str) is None:
+        return position - last_direction
+    else:
+        return position
 
 
 def translate_position_to_button_number(position):
@@ -54,7 +69,8 @@ def translate_position_to_button_number(position):
     (-1, -1) 7  ( 0, -1) 8  ( 1, -1) 9
     """
     x, y = position[0], position[1]
-    button_number = (x + 2) + (1 - y) * 3
+    position_str = '(%d, %d)' % (x, y)
+    button_number = BUTTON_NUMBER_MAP.get(position_str)
 
     return button_number
 
