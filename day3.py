e71df4e59745ceb04f0b373e1e631e2af81eb17f
jianming.wu <jianming.wu@yingzt.com>
2017-05-22 22:46:48 +0800

    finish day 3 part 2

--- day3.py
+++ day3.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pandas as pd
+
+
 def main():
     """入口"""
-    nr_triangle = 0
+    data_frame = pd.read_table('day3.txt', delim_whitespace=True, header=None)
 
-    for line in open('day3.txt'):
-        line = line.strip()
-        a, b, c = line.split()
-        a, b, c = int(a), int(b), int(c)
+    nr_row_triangle = 0
 
-        if is_valid_triangle(a, b, c):
-            nr_triangle += 1
+    for row in data_frame.values:
+        for idx in range(0, len(row), 3):
+            a, b, c = row[idx:idx+3]
 
-    print nr_triangle
+            nr_row_triangle += is_valid_triangle(a, b, c)
+
+    print nr_row_triangle
+
+    nr_column_triangle = 0
+
+    for column in data_frame.T.values:
+
+        for idx in range(0, len(column), 3):
+            a, b, c = column[idx:idx+3]
+
+            nr_column_triangle += is_valid_triangle(a, b, c)
+
+    print nr_column_triangle
 
 
 def is_valid_triangle(a, b, c):
@@ -23,9 +37,11 @@ def is_valid_triangle(a, b, c):
     即   a + b + c > 2c
     即   (a + b + c) > (2 * max(a, b, c))
     """
+    print type(a)
     max_side = max(a, b, c)
+    is_valid = (a + b + c) > (2 * max_side)
 
-    return (a + b + c) > (2 * max_side)
+    return is_valid
 
 
 if __name__ == '__main__':
