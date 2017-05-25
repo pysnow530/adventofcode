7a38bd76c551eb181aac47d1d1a4c5154c9d254c
jianming.wu <jianming.wu@yingzt.com>
2017-05-25 22:37:25 +0800

    finish day 6 part 1

--- day5.py
+++ day5.py
@@ -4,18 +4,54 @@ import hashlib
 import itertools
 
 
-door_id = 'cxdnnyjw'
-password_length = 8
+def main():
+    """入口"""
+    door_id = 'cxdnnyjw'
+    password_length = 8
 
 
-password = ''
+    valid_hashes = []
 
-for i in itertools.count(0):
-    hexdigest = hashlib.md5(door_id + str(i)).hexdigest()
-
-    if hexdigest.startswith('00000'):
-        password += hexdigest[5]
-
-        if len(password) >= password_length:
-            print password
+    for i in itertools.count(0):
+        if password2_cracked(valid_hashes):
             break
+
+        hexdigest = hashlib.md5(door_id + str(i)).hexdigest()
+
+        if hexdigest.startswith('00000'):
+            valid_hashes.append(hexdigest)
+
+    print calculate_password1(valid_hashes), calculate_password2(valid_hashes)
+
+
+def password2_cracked(valid_hashes):
+    """判断password2是否已破解"""
+    positions = [h[5] for h in valid_hashes]
+    position_filled_list = map(positions.__contains__, '01234567')
+
+    return all(position_filled_list)
+
+
+def calculate_password1(valid_hashes):
+    """计算password1"""
+    return ''.join([i[5] for i in valid_hashes[:8]])
+
+
+def calculate_password2(valid_hashes):
+    """计算password2"""
+    password_chars = [None] * 8
+
+    for valid_hash in valid_hashes:
+        position, password_char = valid_hash[5:7]
+
+        if position.isdigit():
+            position = int(position)
+            if position < 8 and password_chars[position] is None:
+                password_chars[position] = password_char
+
+        if None not in password_chars:
+            return ''.join(password_chars)
+
+
+if __name__ == '__main__':
+    main()
---
+++ day6.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import string
+import collections
+
+import pandas as pd
+
+
+def main():
+    """入口"""
+    # 获取字符矩阵
+    lines = open('day6.txt').readlines()
+    lines = map(string.strip, lines)
+    char_matrix = map(list, lines)
+
+    # 转置
+    data_frame = pd.DataFrame(char_matrix)
+    char_matrix_t = data_frame.T.values
+
+    # 连接最大出现次数的字符
+    print ''.join([collections.Counter(i).most_common(1)[0][0]
+                  for i in char_matrix_t])
+
+
+if __name__ == '__main__':
+    main()
