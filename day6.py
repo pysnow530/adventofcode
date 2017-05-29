b47762bcb0e4b3abd81d49f9771b41758446bbf9
jianming.wu <jianming.wu@yingzt.com>
2017-05-29 16:40:27 +0800

    finish day 6 part 2

--- day6.py
+++ day6.py
@@ -18,7 +18,11 @@ def main():
     char_matrix_t = data_frame.T.values
 
     # 连接最大出现次数的字符
-    print ''.join([collections.Counter(i).most_common(1)[0][0]
+    print ''.join([collections.Counter(i).most_common()[0][0]
+                  for i in char_matrix_t])
+
+    # 连接最小出现次数的字符
+    print ''.join([collections.Counter(i).most_common()[-1][0]
                   for i in char_matrix_t])
 
 
