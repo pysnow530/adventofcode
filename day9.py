7cd145f60b4a661ed1fdf4b5061fefcc8c92a702
jianming.wu <jianming.wu@yingzt.com>
2017-06-20 21:55:10 +0800

    finish day 9 part 2

--- day9.py
+++ day9.py
@@ -3,11 +3,15 @@
 import re
 
 
+RE_MARKER = re.compile(r'^(\((\d+)x(\d+)\))')
+
+
 def main():
     """入口"""
     s = open('day9.txt').read().strip()
 
     print count_depressed_length(s)
+    print count_depressed_length2(s)
 
 
 def count_depressed_length(s):
@@ -19,18 +23,40 @@ def count_depressed_length(s):
 
     if parse_result is not None:
         marker_length, repeat_length, repeat_times = parse_result
-        return (repeat_length*repeat_times +
+        return (repeat_length * repeat_times +
                 count_depressed_length(s[marker_length+repeat_length:]))
 
     else:
         return 1 + count_depressed_length(s[1:])
 
 
+def count_depressed_length2(s):
+    """统计解密后的文本长度（支持递归展开）"""
+    count = 0
+
+    while s != '':
+        parse_result = try_parse_marker(s)
+
+        if parse_result is not None:
+            marker_length, repeat_length, repeat_times = parse_result
+
+            repeat_text = s[marker_length:marker_length+repeat_length]
+            count += count_depressed_length2(repeat_text) * repeat_times
+
+            s = s[marker_length+repeat_length:]
+
+        else:
+            count += 1
+            s = s[1:]
+
+    return count
+
+
 def try_parse_marker(s):
     """尝试解析s头的marker，如果不是marker，返回None，否则返回
     (marker_length, repeat_length, repeat_times)
     """
-    re_matched = re.match(r'^(\((\d+)x(\d+)\))', s)
+    re_matched = RE_MARKER.match(s)
 
     if re_matched:
         marker, repeat_length, repeat_times = re_matched.groups()
